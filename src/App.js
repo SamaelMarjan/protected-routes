d58ade4a67caf43d0fd2a4194194119ce6b53686
Samael Marjan <samaaelmarjan@gmail.com>
2022-10-12 10:36:10 +0600

    update-oct-12-10-36

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Accounts from './components/Accounts';
 
 function App() {
   return (
     <>
-      App
+      <div>
+        <h1 className='text-center text-3xl font-bold'>
+          Firebase Auth & Context
+        </h1>
+        <Routes>
+          <Route path='/' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/account' element={<Accounts />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/components/Signin.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Signin = () => {
+  return (
+    <div className='bg-blue-100 max-w-[700px] mx-auto my-16 p-4 rounded-lg'>
+
+      <div>
+        <h1 className='text-2xl font-bold py-2'>
+          Sign in to your account
+        </h1>
+        <p className='py-2'>
+          Don't have an account yet?
+          <Link to='/signup' className='underline'>Sign up</Link>
+        </p>
+      </div>
+
+      <from>
+        
+        <div className='flex flex-col py-2'>
+          <label className='py-2 font-medium'>E-mail Address</label>
+          <input className='border outline-none p-2' type='email' />
+        </div>
+
+        <div className='flex flex-col py-2'>
+          <label className='py-2 font-medium'>Password</label>
+          <input className='border outline-none p-2' type='password' />
+        </div>
+
+        <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-2 my-4 text-white'>Sign In</button>
+
+      </from>
+
+    </div>
+  );
+}
+
+export default Signin;
